0301c0f4afe82d5babc5e7816683db77431383b0
Gabriel <furstenheim@gmail.com>
2022-02-14 17:50:50 +0100

    Improve definition

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -10,8 +10,8 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        Guess the word in 6 tries. After each guess, the color of the tiles will
-        change to show how close your guess was to the word.
+        Chase the word. After each guess, the color of the tiles will
+        change to show how close your guess was to the word. Only the last hints are considered.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
